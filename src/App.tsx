ed09fe2d7ffdb9ca96632c910436cfbfac58c857
PavanYadavGo <Pavan3253@outlook.com>
2026-08-30 10:19:59 +0530

    SYS:Otp auth fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { auth } from "./firebase";
+// import { auth } from "./firebase";
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Toaster } from 'sonner';
@@ -22,7 +22,7 @@ const ResetPasswordPage = lazy(() => import('./pages/ResetPasswordPage'));
 const VerifyEmailPage = lazy(() => import('./pages/VerifyEmailPage'));
 const AddPropertyPage = lazy(() => import('./pages/AddPropertyPage'));
 const MyListingsPage = lazy(() => import('./pages/MyListingsPage'));
-console.log(auth);
+// console.log(auth);
 
 function NotFoundPage() {
   return (
